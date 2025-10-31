3514ded6ff679a791226adc31806b98fe5626130
pjdmartinez <pjdmartinez15.research@gmail.com>
2025-10-30 23:49:21 -0400

    progreso enorme

---
+++ tests/source/MidiProcessingTest.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include <AudioProcessor.h>
+namespace audio_plugin_test{
+    TEST(AudioPlugin, Foo){
+        AudioPluginAudioProcessor processor();
+        ASSERT_FALSE(true);
+    }
+}
